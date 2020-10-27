806e471d41b38068678e4c6ee9dc916fabe10fbb
manuel <mminuti3@gmail.com>
2020-10-27 22:57:55 +0100

    chore(): first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+require('dotenv').config()
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('{message:"'+ process.env.MESSAGE || "Hello world" +''"}');
+});
+
+server.listen(process.env.PORT);
+
+console.log("The app is listening on port "+ process.env.PORT)
